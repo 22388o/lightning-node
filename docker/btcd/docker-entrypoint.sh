4da6bff3a51517f809e3396b0d70c93bdb93ffe9
Joel Cumberland <joel_c@zoho.com>
2018-10-31 08:14:56 +1100

    Set RPC IP to have a default ENV value

--- docker/btcd/docker-entrypoint.sh
+++ docker/btcd/docker-entrypoint.sh
@@ -7,14 +7,14 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
     if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
 	cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
       printtoconsole=1
-      rpcallowip=::/0
+      rpcallowip=${BITCOIN_RPC_ALLOWED:-127.0.0.1}
       rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
       rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 
 	EOF
         chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
     fi
-
+    
     # ensure correct ownership and linking of data directory
     # we do not update group ownership here, in case users want to mount
     # a host directory and still retain access to it
