5225fbdd8bdd4a6c9b1f1075cdc7218fbe264f03
Joel Cumberland <joel_c@zoho.com>
2018-11-01 09:51:16 +1100

    supports .env file in docker container

--- bin/bitcoind.sh
+++ bin/bitcoind.sh
@@ -5,7 +5,9 @@ source ${pwd/}init.sh #TODO: fix this so the dir is set as a var
 ARG=$1
 
 function start() {
+    touch ${MNTVOL}/${BTCNAME}/.env
     docker run --rm --name ${BTCNAME} -d \
+        --env-file ${MNTVOL}/${BTCNAME}/.env \
         -v ${MNTVOL}/${BTCNAME}:/home/$RUNAS/.bitcoin \
         -p 0.0.0.0:9735:9735 \
         -p 127.0.0.1:28332:28332 \
@@ -14,7 +16,6 @@ function start() {
 }
 
 function stop() {
-    # docker exec-it  bitcoind_mainnet bitcoin-cli stop
     docker stop ${BTCNAME}
 }
 
--- bin/lnd.sh
+++ bin/lnd.sh
@@ -5,8 +5,10 @@ source ${pwd/}init.sh #TODO: fix this so the dir is set as a var
 ARG=$1
 
 function start() {
+    touch ${MNTVOL}/${LNDNAME}/.env
     # waitforblock
     docker run --rm --name ${LNDNAME} --network container:${BTCNAME} -d \
+        --env-file ${MNTVOL}/${LNDNAME}/.env \
         -v ${MNTVOL}/${BTCNAME}:/home/$RUNAS/.bitcoin \
         -v ${MNTVOL}/${LNDNAME}:/home/$RUNAS/.lnd \
         ${LNDIMAGE}
