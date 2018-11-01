bbd915cb98c7a2815d575b3b811084f48c6ae308
Joel Cumberland <joel_c@zoho.com>
2018-11-01 12:31:16 +1100

    use a cleaner script

--- docker/lnd/docker-entrypoint.sh
+++ docker/lnd/docker-entrypoint.sh
@@ -3,48 +3,13 @@
 # exit from script if error was raised.
 set -e
 
-# error function is used within a bash function in order to send the error
-# message directly to the stderr output and exit.
-error() {
-    echo "$1" > /dev/stderr
-    exit 0
-}
-
-# return is used within bash function in order to return the value.
-return() {
-    echo "$1"
-}
-
-# set_default function gives the ability to move the setting of default
-# env variable from docker file to the script thereby giving the ability to the
-# user override it durin container start.
-set_default() {
-    # docker initialized env variables with blank string and we can't just
-    # use -z flag as usually.
-    BLANK_STRING='""'
-
-    VARIABLE="$1"
-    DEFAULT="$2"
-
-    if [[ -z "${VARIABLE}" || "${VARIABLE}" == "${BLANK_STRING}" ]]; then
-
-        if [ -z "${DEFAULT}" ]; then
-            error "You should specify default variable"
-        else
-            VARIABLE="${DEFAULT}"
-        fi
-    fi
-
-   return "${VARIABLE}"
-}
-
 # Set default variables if needed.
-BITCOIN_RPC_USER=$(set_default "${BITCOIN_RPC_USER}" "bitcoin")
-BITCOIN_RPC_PASSWORD=$(set_default "${BITCOIN_RPC_PASSWORD}" "password")
-DEBUG=$(set_default "${DEBUG}" "debug")
-NETWORK=$(set_default "${NETWORK}" "mainnet")
-CHAIN=$(set_default "${CHAIN}" "bitcoin")
-BACKEND=$(set_default "${BACKEND}" "bitcoind")
+BITCOIN_RPC_USER=${BITCOIN_RPC_USER:-bitcoin}
+BITCOIN_RPC_PASSWORD=${BITCOIN_RPC_PASSWORD:-password}
+DEBUG=${DEBUG:-debug}
+NETWORK=${NETWORK:-mainnet}
+CHAIN=${CHAIN:-bitcoin}
+BACKEND=${BACKEND:-bitcoind}
 if [[ "${CHAIN}" == "litecoin" ]]; then
     BACKEND="ltcd"
 fi
