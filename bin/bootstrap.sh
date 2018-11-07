d0f4b03d9f426efa7342b68269b0fc606544532c
Joel Cumberland <joel_c@zoho.com>
2018-11-08 05:39:20 +1100

    use bitcoin home dir

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -20,7 +20,7 @@ git clone https://github.com/jrosco/lightning-node.git /opt/lightning-node
 cd /opt/lightning-node && source bin/init.sh
 
 # mount btcd and lnd data volume (create vol in sf region)
-BTCDATADIR=/scratch/bitcoin/mainnet/
+BTCDATADIR=/home/bitcoin/mainnet/
 mkdir -p ${BTCDATADIR}
 echo "/dev/disk/by-id/scsi-0DO_Volume_volume-sfo2-01 ${BTCDATADIR} ext4 defaults,nofail,discard 0 0" | sudo tee -a /etc/fstab
 mount -a
