ab5547dc361252709ee8ceb5a7bd8c204a0a835a
Joel Cumberland <joel_c@zoho.com>
2018-11-11 15:27:14 +1100

    add bin script

---
+++ bin/electrumx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source $(pwd)/init.sh #TODO: fix this so the dir is set as a var
+
+ARG=$1
+
+function start() {
+    touch ${MNTVOL}/${ELECXNAME}/.env
+    waitforblock
+    docker run --rm --name ${ELECXNAME} --network container:${BTCNAME} -d \
+        --env-file ${MNTVOL}/${ELECXNAME}/.env \
+        -v ${MNTVOL}/${BTCNAME}:/home/$RUNAS/.bitcoin \
+        ${LNDIMAGE}
+}
+
+function stop() {
+    docker stop ${ELECXNAME}
+}
+
+function restart() {
+    stop; sleep 5; start
+}
+
+function status() {
+    docker ps -f name=${ELECXNAME}
+}
+
+case ${ARG} in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    status)
+        status
+        ;;
+    *)
+esac
