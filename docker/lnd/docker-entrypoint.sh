8804f5361786137bbf079cf1374e2f13e29efcf6
Joel C <joel_c@zoho.com>
2018-12-21 08:48:30 +1100

    added testnet ports and zmq ports
    enable mainnet by default
    
    refactor entrypoint params
    
    use sh shebang

--- docker/lnd/docker-entrypoint.sh
+++ docker/lnd/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # exit from script if error was raised.
 set -e
@@ -6,8 +6,8 @@ set -e
 # Set default variables if needed.
 BITCOIN_RPC_USER=${BITCOIN_RPC_USER:-bitcoin}
 BITCOIN_RPC_PASSWORD=${BITCOIN_RPC_PASSWORD:-password}
-DEBUG=${DEBUG:-debug}
-NETWORK=${NETWORK:-testnet}
+DEBUG=${DEBUG:-info}
+NETWORK=${NETWORK:-mainnet}
 CHAIN=${CHAIN:-bitcoin}
 BACKEND=${BACKEND:-bitcoind}
 ZMQ_PUB_RAW_TX=${ZMQ_PUB_RAW_TX:-"tcp://127.0.0.1:8332"}
@@ -18,34 +18,30 @@ fi
 
 mkdir -p "${LIGHTNING_DATA}"
 
+PARAMS=$(echo \
+    "--${CHAIN}.active" \
+    "--${CHAIN}.${NETWORK}" \
+    "--${CHAIN}.node=${BACKEND}" \
+    "--${BACKEND}.rpcuser=${BITCOIN_RPC_USER}" \
+    "--${BACKEND}.rpcpass=${BITCOIN_RPC_PASSWORD}" \
+    "--${BACKEND}.zmqpubrawblock=${ZMQ_PUB_RAW_TX}" \
+    "--${BACKEND}.zmqpubrawtx=${ZMQ_PUB_RAW_BLK}" \
+    "--debuglevel=${DEBUG}" \
+    "--configfile=${LIGHTNING_DATA}/lnd.conf"
+)
+
 if [[ ! -s "${LIGHTNING_DATA}/lnd.conf" ]]; then
 cat <<-EOF > "${LIGHTNING_DATA}/lnd.conf"
-        [Application Options]
-        debuglevel=${DEBUG}
-        maxpendingchannels=5
+    [Application Options]
+    maxpendingchannels=5
 
-        [Bitcoin]
-        bitcoin.active=1
-        # enable either testnet or mainnet
-        bitcoin.${NETWORK}=1
-        bitcoin.node=${BACKEND}
-
-        [autopilot]
-        autopilot.active=1
-        autopilot.maxchannels=5
-        autopilot.allocation=0.6
+    [autopilot]
+    autopilot.active=1
+    autopilot.maxchannels=5
+    autopilot.allocation=0.6
 EOF
     chown -R 1000:1000 "${LIGHTNING_DATA}/"
 fi
 
-su bitcoin -c "lnd \
-    --noseedbackup \
-    --${CHAIN}.active \
-    --${CHAIN}.${NETWORK} \
-    --${CHAIN}.node=${BACKEND} \
-    --${BACKEND}.rpcuser=${BITCOIN_RPC_USER} \
-    --${BACKEND}.rpcpass=${BITCOIN_RPC_PASSWORD} \
-    --${BACKEND}.zmqpubrawblock=${ZMQ_PUB_RAW_TX} \
-    --${BACKEND}.zmqpubrawtx=${ZMQ_PUB_RAW_BLK} \
-    --debuglevel=${DEBUG} \
+su bitcoin -c "lnd ${PARAMS} \
     $@"
