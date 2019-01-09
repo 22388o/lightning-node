43d7a6667ead42a23a7e0d8334ca56d2569327b6
Joel C <joel_c@zoho.com>
2019-01-09 19:23:33 +1100

    get run directory from within the script

--- bin/bitcoind.sh
+++ bin/bitcoind.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source $(pwd)/init.sh #TODO: fix this so the dir is set as a var
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${DIR}/init.sh
 
 ARG=$1
 
