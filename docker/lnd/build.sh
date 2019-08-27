c5608a3f7d296248cf1ff64cc4a948629d94c5c5
Joel Cumberland <joel_c@zoho.com>
2019-08-27 23:25:34 +1000

    build.sh update (needs more work)

--- docker/lnd/build.sh
+++ docker/lnd/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-LND_VERSION=$(grep LND_VERSION= Dockerfile | cut -d= -f2)
-docker build -t jr0sco/lnd .
-docker tag jr0sco/lnd jr0sco/lnd:lnd-${LND_VERSION} 
-docker push jr0sco/lnd:lnd-${LND_VERSION}
+LND_VERSION="v0.7.1-beta"
+LOOP_VERSION="v0.2.2-alpha"
+
+docker build --build-arg LND_VERSION=${LND_VERSION} --build-arg LOOP_VERSION=${LOOP_VERSION} -t lnd .
+
+docker tag jr0sco/lnd jr0sco/lnd:${LND_VERSION}
+docker push jr0sco/lnd:${LND_VERSION}
